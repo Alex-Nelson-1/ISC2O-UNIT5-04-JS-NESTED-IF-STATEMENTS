58e01ada4765590743a662fae7d5d644e4b16236
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-12-08 18:47:57 +0000

    Checks if the angles add up to 180

--- js/script.js
+++ js/script.js
@@ -16,6 +16,20 @@ if (navigator.serviceWorker) {
 /**
  * This function displays an alert.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function calculateArea() {
+  const lengthA = parseFloat(document.getElementById("sideLengthA").value)
+  const lengthB = parseFloat(document.getElementById("sideLengthB").value)
+  const lengthC = parseFloat(document.getElementById("sideLengthC").value)
+
+  
+  const angleA = Math.acos((lengthB**2 + lengthC**2 - lengthA**2) / (2 * lengthB * lengthC)) * (180/Math.PI)
+  const angleB = Math.acos((lengthC**2 + lengthA**2 - lengthB**2) / (2 * lengthC * lengthA)) * (180/Math.PI)
+  const angleC = Math.acos((lengthA**2 + lengthB**2 - lengthC**2) / (2 * lengthA * lengthB)) * (180/Math.PI)
+
+  const sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + Number((angleC).toFixed(2))
+  
+  document.getElementById("angleA").innerHTML = "Angle A = " + angleA + "°"
+  document.getElementById("angleB").innerHTML = "Angle B = " + angleB + "°"
+  document.getElementById("angleC").innerHTML = "Angle C = " + angleC + "°"
+  document.getElementById("angleSum").innerHTML = sumOfAngles + "°"
 }
